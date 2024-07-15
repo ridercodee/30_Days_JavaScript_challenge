1d375fe673ecd732e86b2e4224dff58d0a037ddd
Mehboob Khan <mehboobrider57@gmail.com>
2024-07-15 16:06:32 +0530

    Add files via upload

---
+++ Day_02.js
@@ -0,0 +1,81 @@
+// Day 2: Operators
+console.log("**Activity 1**");
+
+// Task _1 // Adding 2 Numbers
+const a = 10;
+const b = 20;
+let result = (a + b);
+console.log(result);
+
+// Task _2 // Subtract 2 Numbers
+const m = 50;
+const n = 20;
+let results = (m - n);
+console.log(results);
+
+// Task _3 // Multiply 2 Numbers
+console.log(a * b);
+
+// Task _4 // Divide 2 Numbers
+console.log(m / n);
+
+// Task _5 // Reminder 2 Numbers
+console.log(m%n);
+
+console.log("**Activity 2**");
+
+// Task _6 // Assignment operator add
+let add = 10
+console.log(add += 2);
+
+// Task _7 // Assignment operator substrct
+let sub = 20;
+console.log(sub -= 8);
+
+console.log("**Activity 3**");
+
+// Task _8 // Comparison operators
+let num3 = 35;
+let num4 = 64;
+const resultt = (num3 < num4)  
+console.log(num3 > num4);
+console.log(resultt);
+
+// Task _9 // comparison operator
+if (num3 <= num4) { 
+  console.log('True')
+}else { 
+  console.log('False')
+};
+
+// Task _10 // Comparison operator
+console.log('comparing NUmbers using "==": ', 10 == "10");
+console.log('comparing NUmbers using "===": ', 10 === "10");
+
+
+console.log("**Activity 4**");
+
+// Task _11 // Logical operator
+if (a+b && n-m == 30) {
+  console.log('True')
+} else {
+  console.log('Both conditions are False')
+};
+
+// Task _12 // Logical operators
+if (a+b || n-m == 30) {
+  console.log('True')
+} else {
+  console.log('Both conditions are False')
+};
+
+// Task _13 // Logical operators
+let num = "2";
+console.log( num != "2");
+
+
+console.log("**Activity 5**")
+
+// Task _14 // Logical operators
+let num6 = 5;
+console.log(5>0 ? 'positive' : 'negative');
